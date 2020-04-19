68421fb559502c86da9186b9e2f943e13e1220ac
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-04-19 20:53:30 +0100

    Increments per second + Memory leak prevention

--- App.js
+++ App.js
@@ -4,9 +4,31 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Timer></Timer>
     </View>
   );
+}
+
+class Timer extends React.Component{
+  state = {
+    time: 0,
+  }
+
+  constructor(){
+    super()
+  }
+
+  componentDidMount() {
+    this.timeFunction = setInterval(() => {console.log(this.state.time); this.setState({time: this.state.time + 1})}, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeFunction)
+  }
+
+  render(){
+    return (<Text>{this.state.time}</Text>)
+  }
 }
 
 const styles = StyleSheet.create({
